85bf7498cf5cef76deb3d5cad1bffcb1ae9a41d5
alexanderelmuratov <elmuratov@gmail.com>
2023-03-31 15:42:01 +0300

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import {
   checkAuth,
@@ -26,6 +27,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post(
