5790519181c5bc25fa3dbb0ab6cbf37568361ac0
alexanderelmuratov <elmuratov@gmail.com>
2023-05-26 19:16:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 import {
   checkAuth,
